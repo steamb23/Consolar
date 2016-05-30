2fe6e2c62624c93cdc6833a81f209d6067de20f1
steamb23 <steamb23@gmail.com>
2016-05-31 02:03:29 +0900

    using 정리

--- Consolar/IPresentable.cs
+++ Consolar/IPresentable.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace SteamB23.Consolar
+﻿namespace SteamB23.Consolar
 {
     public interface IPresentable
     {
