9302cda7a07a4e5e542f7acea773da6dcaa7323e
steamb23 <steamb23@gmail.com>
2016-05-31 12:45:53 +0900

    KeyInput 이벤트 호출

--- Consolar/Input.cs
+++ Consolar/Input.cs
@@ -16,6 +16,7 @@ namespace SteamB23.Consolar
                 {
                     Console.ReadKey(true);
                 }
+                OnKeyInput(keyInfo);
             }
             else
             {
