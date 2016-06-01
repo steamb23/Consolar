970f1f91b8ec933d781603cfda8f86cf65c0c591
steamb23 <steamb23@gmail.com>
2016-06-02 02:01:59 +0900

    Scene 기능 추가

---
+++ Consolar/Scene.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SteamB23.Consolar
+{
+    public abstract class Scene
+    {
+        public abstract void Run();
+    }
+}
---
+++ Consolar/SceneManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SteamB23.Consolar
+{
+    public class SceneManager
+    {
+        Scene currentScene;
+        Scene nextScene;
+
+        bool isSceneRun = false;
+
+        public Scene CurrentScene
+        {
+            get
+            {
+                return currentScene;
+            }
+        }
+        public Scene NextScene
+        {
+            get
+            {
+                return nextScene;
+            }
+
+            set
+            {
+                this.nextScene = value;
+            }
+        }
+        public void SceneRun()
+        {
+            if (!isSceneRun)
+            {
+                while (nextScene != null)
+                {
+                    currentScene = nextScene;
+                    nextScene = null;
+                    CurrentScene.Run();
+                }
+            }
+        }
+    }
+}
